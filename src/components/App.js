1fdb1b90dfd2db1668571e4b19e191b83722f87d
Timothywaweru1 <timothy.waweru1@student.moringaschool.com>
2025-04-11 13:37:09 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,4 @@
-// import React from "react";
-// import blogData from "../data/blog";
-// import Header from "./Header";
-// import About from "./About";
-// import ArticleList from "./ArticleList";
-// console.log(blogData);
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <About about = {blogData.about} img ={blogData.image}/>
-//       <Header name = {blogData.name}/>
-//       <ArticleList pst = {blogData.posts}/>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React from "react";
 import Header from "./Header";
 import About from "./About";
