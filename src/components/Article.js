57e2c6966818b5305a0d1f6521dd9b381c20a4bb
Timothywaweru1 <timothy.waweru1@student.moringaschool.com>
2025-04-11 13:47:43 +0300

    Update Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,16 +1,4 @@
-// import React from 'react'
 
-// function Article({title,date="January 1,1970",preview}) {
-//   return (
-//     <article>
-//        <h3>{title}</h3>
-//        <small>{date}</small>
-//        <p>{preview}</p>
-//     </article>
-//   )
-// }
-
-// export default Article
 import React from "react";
 
 function Article({ title, date = "January 1, 1970", preview }) {
