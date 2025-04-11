e5b912c9a14daa0e7a2b645259609d0ce7575e94
Timothywaweru1 <timothy.waweru1@student.moringaschool.com>
2025-04-11 13:47:14 +0300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,4 @@
-// import React from 'react'
 
-// const About = ({image ="https://via.placeholder.com/215",about }) => {
-//   return (
-//     <aside>
-//         <img src={image} alt="blog logo"></img>
-//         <p>{about}</p>
-//     </aside>
-//   )
-// }
-
-// export default About
 import React from "react";
 
 function About({ image = "https://via.placeholder.com/215", about = "This is a blog about learning React." }) {
